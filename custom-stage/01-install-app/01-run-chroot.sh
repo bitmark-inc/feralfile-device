4357174d79f9b8edb32e8708ac2f79f2fd743624
Brandon Yeh <lpopo0856@gmail.com>
2025-01-03 18:15:25 +0800

    try out open box
    nx
    
    lower down the fonts
    
    fallback
    
    fallback again
    
    bring back needed packages
    
    add gvfs back
    
    bring back desktop
    
    f
    
    use openbox again
    
    build one with no auto settings
    
    try first version
    
    remove lightdm
    
    fix
    
    Revert "remove lightdm"
    
    This reverts commit 57245758b15ab5b8cb00b44918fa66c800a3092f.
    
    push a stable version
    
    try to auto connect to the wifi
    
    Revert "try to auto connect to the wifi"
    
    This reverts commit eeefccf562ab14d6298089f8a8e54b673e182b8a.
    
    fix

--- custom-stage/01-install-app/01-run-chroot.sh
+++ custom-stage/01-install-app/01-run-chroot.sh
@@ -2,15 +2,11 @@
 
 dpkg -i feralfile-launcher_arm64.deb
 
-# Create autostart directory and desktop entry
-mkdir -p /home/feralfile/.config/autostart
-cat > /home/feralfile/.config/autostart/feralfile-launcher.desktop <<DESKTOP
-[Desktop Entry]
-Type=Application
-Name=Feral File Launcher
-Exec=/opt/feralfile/feralfile
-X-GNOME-Autostart-enabled=true
-DESKTOP
+# Create autostart
+mkdir -p /home/feralfile/.config/openbox
+cat > /home/feralfile/.config/openbox/autostart <<EOF
+/opt/feralfile/feralfile &
+EOF
 
 # Set correct ownership
 chown -R feralfile:feralfile /home/feralfile/.config
