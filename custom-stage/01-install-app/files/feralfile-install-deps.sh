15241fdc805ab10daeebcd7d151ff17f228c0750
Brandon Yeh <lpopo0856@gmail.com>
2025-02-07 17:38:24 +0800

    fix the wrong script

--- custom-stage/01-install-app/files/feralfile-install-deps.sh
+++ custom-stage/01-install-app/files/feralfile-install-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt-get update
-apt-get -y install $(apt-cache depends feralfile-launcher | awk '{print $2}' | grep -v '<' | xargs)
+apt-get -y install $(apt-cache depends feralfile-launcher | awk '{print $2}' | sed 's/[<>]//g' | xargs)
