9c1daa37703bf5d350cce1e03c65bcae04cd1d11
PuPha <nguyensanghso@gmail.com>
2025-02-14 11:47:48 +0700

    Update Timezone

--- launcher-app/lib/services/commands/set_time_handler.dart
+++ launcher-app/lib/services/commands/set_time_handler.dart
@@ -1,4 +1,5 @@
 import 'package:dbus/dbus.dart';
+import 'package:process_run/stdio.dart';
 
 import '../bluetooth_service.dart';
 import '../logger.dart';
@@ -47,75 +48,22 @@ class SetTimeHandler implements CommandHandler {
 }
 
 class SetTimezoneHandler implements CommandHandler {
-  Future<Map<String, dynamic>> _verifyTimezone(
-      DBusClient client, String expectedTimezone) async {
-    try {
-      final path = DBusObjectPath('/org/freedesktop/timedate1');
-      final result = await client.callMethod(
-        destination: 'org.freedesktop.timedate1',
-        path: path,
-        interface: 'org.freedesktop.DBus.Properties',
-        name: 'Get',
-        values: [
-          const DBusString('org.freedesktop.timedate1'),
-          const DBusString('Timezone'),
-        ],
-      );
-
-      final currentTimezone =
-          (result.values.first as DBusVariant).value.asString();
-      final now = DateTime.now();
-      final offset = now.timeZoneOffset.inHours;
-      final currentOffset = offset >= 0 ? '+$offset' : '$offset';
-
-      return {
-        'success': currentTimezone == expectedTimezone,
-        'current_timezone': currentTimezone,
-        'expected_timezone': expectedTimezone,
-        'offset': currentOffset,
-      };
-    } catch (e) {
-      return {
-        'success': false,
-        'error': 'Failed to verify timezone: ${e.toString()}'
-      };
-    }
-  }
-
   @override
   Future<void> execute(
       Map<String, dynamic> data, BluetoothService bluetoothService,
       [String? replyId]) async {
     try {
-      final timezone = 'Asia/Ho_Chi_Minh'; //data['timezone'] as String;
-      final path = DBusObjectPath('/org/freedesktop/timedate1');
-
-      final client = DBusClient.system();
+      final timezone = data['timezone'] as String;
       try {
-        await client.callMethod(
-          destination: 'org.freedesktop.timedate1',
-          path: path,
-          interface: 'org.freedesktop.timedate1',
-          name: 'SetTimezone',
-          values: <DBusValue>[
-            DBusString(timezone),
-            const DBusBoolean(false),
-          ],
-        );
-
-        try {
-          logger.info('Verifying timezone');
-          final result = await _verifyTimezone(client, timezone);
-          logger.info('Timezone verification result: $result');
-        } catch (e) {
-          logger.severe('Error verifying timezone: $e');
-        }
-
+        var result = await Process.run(
+            'sudo', ['timedatectl', 'set-timezone', timezone]);
+        logger.info('Set timezone result: ${result.stdout}');
         if (replyId != null) {
           bluetoothService.notify(replyId, {'success': true});
         }
-      } finally {
-        await client.close();
+      } catch (e) {
+        logger.severe('Error setting timezone: $e');
+        rethrow;
       }
     } catch (e) {
       logger.severe('Error setting timezone: $e');
