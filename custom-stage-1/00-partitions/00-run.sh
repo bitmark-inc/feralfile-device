f687b0b1462696a4c81b454b4386125b0d3a790d
Anh Nguyen <anhnguyen@bitmark.com>
2025-02-21 10:29:04 +0700

    align the partition

--- custom-stage-1/00-partitions/00-run.sh
+++ custom-stage-1/00-partitions/00-run.sh
@@ -24,31 +24,39 @@ mkdir -p "${ROOTFS_DIR}"
 # All partition sizes and starts will be aligned to this size
 ALIGN="$((4 * 1024 * 1024))"
 
+# Calculate partition sizes in bytes
 BOOT_SIZE="$((256 * 1024 * 1024))"
 UPDATE_SIZE="$((2048 * 1024 * 1024))"
 BUSYBOX_SIZE="$((512 * 1024 * 1024))"
 ROOT_SIZE=$(du -x --apparent-size -s "${EXPORT_ROOTFS_DIR}" --exclude var/cache/apt/archives --exclude boot/firmware --block-size=1 | cut -f 1)
 ROOT_MARGIN="$(echo "($ROOT_SIZE * 0.2 + 200 * 1024 * 1024) / 1" | bc)"
 
-# Calculate partition starts and sizes
-BOOT_PART_START=$((ALIGN))
-BOOT_PART_SIZE=$(((BOOT_SIZE + ALIGN - 1) / ALIGN * ALIGN))
-UPDATE_PART_START=$((BOOT_PART_START + BOOT_PART_SIZE))
-UPDATE_PART_SIZE=$(((UPDATE_SIZE + ALIGN - 1) / ALIGN * ALIGN))
-BUSYBOX_PART_START=$((UPDATE_PART_START + UPDATE_PART_SIZE))
-BUSYBOX_PART_SIZE=$(((BUSYBOX_SIZE + ALIGN - 1) / ALIGN * ALIGN))
-ROOT_PART_START=$((BUSYBOX_PART_START + BUSYBOX_PART_SIZE))
-ROOT_PART_SIZE=$(((ROOT_SIZE + ROOT_MARGIN + ALIGN - 1) / ALIGN * ALIGN))
+# Calculate partition starts and sizes with proper alignment
+BOOT_START=$((ALIGN))
+BOOT_END=$((BOOT_START + BOOT_SIZE))
+BOOT_SIZE_ALIGNED=$(((BOOT_END + ALIGN - 1) / ALIGN * ALIGN))
 
-IMG_SIZE=$((ROOT_PART_START + ROOT_PART_SIZE))
+UPDATE_START=$((BOOT_START + BOOT_SIZE_ALIGNED))
+UPDATE_END=$((UPDATE_START + UPDATE_SIZE))
+UPDATE_SIZE_ALIGNED=$(((UPDATE_END + ALIGN - 1) / ALIGN * ALIGN))
+
+BUSYBOX_START=$((UPDATE_START + UPDATE_SIZE_ALIGNED))
+BUSYBOX_END=$((BUSYBOX_START + BUSYBOX_SIZE))
+BUSYBOX_SIZE_ALIGNED=$(((BUSYBOX_END + ALIGN - 1) / ALIGN * ALIGN))
+
+ROOT_START=$((BUSYBOX_START + BUSYBOX_SIZE_ALIGNED))
+ROOT_END=$((ROOT_START + ROOT_SIZE + ROOT_MARGIN))
+ROOT_SIZE_ALIGNED=$(((ROOT_END + ALIGN - 1) / ALIGN * ALIGN))
+
+IMG_SIZE=$((ROOT_START + ROOT_SIZE_ALIGNED))
 
 truncate -s "${IMG_SIZE}" "${IMG_FILE}"
 
 parted --script "${IMG_FILE}" mklabel msdos
-parted --script "${IMG_FILE}" unit B mkpart primary fat32 "${BOOT_PART_START}" "$((BOOT_PART_START + BOOT_PART_SIZE - 1))"
-parted --script "${IMG_FILE}" unit B mkpart primary ext4 "${UPDATE_PART_START}" "$((UPDATE_PART_START + UPDATE_PART_SIZE - 1))"
-parted --script "${IMG_FILE}" unit B mkpart primary ext4 "${BUSYBOX_PART_START}" "$((BUSYBOX_PART_START + BUSYBOX_PART_SIZE - 1))"
-parted --script "${IMG_FILE}" unit B mkpart primary ext4 "${ROOT_PART_START}" "$((ROOT_PART_START + ROOT_PART_SIZE - 1))"
+parted --script "${IMG_FILE}" unit B mkpart primary fat32 "${BOOT_START}" "$((BOOT_START + BOOT_SIZE_ALIGNED - 1))"
+parted --script "${IMG_FILE}" unit B mkpart primary ext4 "${UPDATE_START}" "$((UPDATE_START + UPDATE_SIZE_ALIGNED - 1))"
+parted --script "${IMG_FILE}" unit B mkpart primary ext4 "${BUSYBOX_START}" "$((BUSYBOX_START + BUSYBOX_SIZE_ALIGNED - 1))"
+parted --script "${IMG_FILE}" unit B mkpart primary ext4 "${ROOT_START}" "$((ROOT_START + ROOT_SIZE_ALIGNED - 1))"
 parted --script "${IMG_FILE}" set 1 boot on
 
 echo "Creating loop device..."
