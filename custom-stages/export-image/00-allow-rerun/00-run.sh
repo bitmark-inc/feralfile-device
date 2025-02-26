14066c26bc34bc61ada52e6fb8fd7e9bc65e8825
Anh Nguyen <anhnguyen@bitmark.com>
2025-02-26 15:17:48 +0700

    fix

--- custom-stages/export-image/00-allow-rerun/00-run.sh
+++ custom-stages/export-image/00-allow-rerun/00-run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash -e
 
-if [ ! -d "${ROOTFS_DIR}" ]; then
-   copy_previous
+# If your script calculates/assigns these:
+IMG_FILE="${STAGE_WORK_DIR}/${IMG_FILENAME}.img"
+LOOP_DEV="$(losetup -j "${IMG_FILE}" | cut -d: -f1)"
+
+# Format root partition (p2) as ext4
+mkfs.ext4 -F -L rootfs "${LOOP_DEV}p2"
+
+# Mount it so $ROOTFS_DIR points to an actual filesystem
+mkdir -p "${ROOTFS_DIR}"
+mount "${LOOP_DEV}p2" "${ROOTFS_DIR}"
+
+# If you need the OS from the previous stage:
+if [ ! -d "${ROOTFS_DIR}/bin" ]; then
+  copy_previous  # or rsync from a prior rootfs
 fi
 
+# Now $ROOTFS_DIR/usr/bin definitely exists
 mkdir -p "${ROOTFS_DIR}/usr/bin"
 cp /usr/bin/qemu-arm-static "${ROOTFS_DIR}/usr/bin/"
-
-if [ ! -x "${ROOTFS_DIR}/usr/bin/qemu-arm-static" ]; then
-	cp /usr/bin/qemu-arm-static "${ROOTFS_DIR}/usr/bin/"
-fi
-
-if [ -e "${ROOTFS_DIR}/etc/ld.so.preload" ]; then
-	mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
-fi
