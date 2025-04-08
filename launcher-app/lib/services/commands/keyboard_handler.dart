becf97be9e6458f90218ab8dd56dd28946ab5ae6
Anh Nguyen <anhnguyen@bitmark.com>
2025-04-08 14:11:50 +0700

    rebind some special keycodes

--- launcher-app/lib/services/commands/keyboard_handler.dart
+++ launcher-app/lib/services/commands/keyboard_handler.dart
@@ -13,12 +13,15 @@ class KeyboardHandler implements CommandHandler {
     logger.info('keyname', keyName);
 
     try {
+      // Map character to xdotool key name
+      final String xdoKey = _mapToXdoKey(keyCode, keyName);
+
       // Using xdotool to send key events to the active window
-      final result = await Process.run('xdotool', ['key', keyName]);
+      final result = await Process.run('xdotool', ['key', xdoKey]);
       if (result.exitCode != 0) {
         logger.warning('Failed to send keyboard event: ${result.stderr}');
       } else {
-        logger.info('Keyboard event sent: keyCode=$keyCode');
+        logger.info('Keyboard event sent: keyCode=$keyCode, xdoKey=$xdoKey');
       }
 
       // Send success notification using replyId if available
@@ -32,6 +35,26 @@ class KeyboardHandler implements CommandHandler {
             .notify(replyId, {'success': false, 'error': e.toString()});
       }
       rethrow;
+    }
+  }
+
+  // Maps character codes to valid xdotool key names
+  String _mapToXdoKey(int keyCode, String keyName) {
+    // Common special characters that need mapping
+    switch (keyCode) {
+      case 32: // Space
+        return 'space';
+      case 9: // Tab
+        return 'Tab';
+      case 13: // Enter/Return
+        return 'Return';
+      case 27: // Escape
+        return 'Escape';
+      case 8: // Backspace
+        return 'BackSpace';
+      default:
+        // For regular alphanumeric characters
+        return keyName;
     }
   }
 }
