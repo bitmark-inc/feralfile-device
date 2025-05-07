6c496325098d97e02404b761088b683939d83420
Anh Nguyen <anhnguyen@bitmark.com>
2025-05-07 13:58:27 +0700

    v1

--- custom-stage/01-install-app/files/migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-/home/feralfile/migrations/bluez.sh
-/home/feralfile/migrations/mesa.sh
-/home/feralfile/migrations/config.sh
-/home/feralfile/migrations/feralfile_service.sh
-/home/feralfile/migrations/launcher.sh
