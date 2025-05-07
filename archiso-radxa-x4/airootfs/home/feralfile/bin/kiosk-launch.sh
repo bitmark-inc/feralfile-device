ad161c72896550e1653c892d8c7a8dceb81cfa96
Brandon Yeh <lpopo0856@gmail.com>
2025-05-07 16:49:16 +0800

    new ver

--- archiso-radxa-x4/airootfs/home/feralfile/bin/kiosk-launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-/usr/bin/cage -s -- /home/feralfile/bin/kiosk.sh &
-wait
--- archiso-radxa-x4/profiledef.sh
+++ archiso-radxa-x4/profiledef.sh
@@ -8,7 +8,7 @@ iso_application="Feral File Launcher"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
@@ -19,6 +19,4 @@ file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/home/feralfile/bin/kiosk.sh"]="1000:1000:755"
-  ["/home/feralfile/bin/kiosk-launch.sh"]="1000:1000:755"
 )
