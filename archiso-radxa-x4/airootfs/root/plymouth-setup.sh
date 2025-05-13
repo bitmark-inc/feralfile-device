667957b7829551d123728dc32f84acdbc9869de7
Anh Nguyen <anhnguyen@bitmark.com>
2025-05-13 13:38:40 +0700

    fix

--- archiso-radxa-x4/airootfs/root/plymouth-setup.sh
+++ archiso-radxa-x4/airootfs/root/plymouth-setup.sh
@@ -11,9 +11,22 @@ if command -v plymouth-set-default-theme &> /dev/null; then
   plymouth-set-default-theme -R feralfile-splash
 fi
 
-# Make sure theme files are in the correct location
+# Create boot directory if it doesn't exist
 mkdir -p /boot/plymouth/themes/feralfile-splash
+
+# Copy theme files to the boot location
 cp -f /usr/share/plymouth/themes/feralfile-splash/* /boot/plymouth/themes/feralfile-splash/
+
+# Make sure theme is set in config file
+if [ -f /etc/plymouth/plymouthd.conf ]; then
+  sed -i 's/^Theme=.*/Theme=feralfile-splash/' /etc/plymouth/plymouthd.conf
+else
+  mkdir -p /etc/plymouth
+  echo "[Daemon]" > /etc/plymouth/plymouthd.conf
+  echo "Theme=feralfile-splash" >> /etc/plymouth/plymouthd.conf
+  echo "ShowDelay=0" >> /etc/plymouth/plymouthd.conf
+  echo "DeviceTimeout=8" >> /etc/plymouth/plymouthd.conf
+fi
 
 # Regenerate initramfs with Plymouth support
 mkinitcpio -P
