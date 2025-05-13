3333b60965cc769ee83968de9665283e38652e60
Brandon Yeh <lpopo0856@gmail.com>
2025-05-13 18:00:47 +0800

    v2

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-updater.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-updater.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+if ! ping -q -c 1 -W 2 archlinux.org >/dev/null; then
+  echo "No network connection. Aborting update."
+  exit 1
+fi
+
 output=$(sudo pacman -Sy --needed --noconfirm feral-connectd feral-setupd)
 
 if ! echo "$output" | grep -q "there is nothing to do"; then
