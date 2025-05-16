bc1ad85ece72d9d712d86a467462057bf5e32d00
Hiếu Phạm <hieupham@bitmark.com>
2025-05-16 18:07:26 +0700

    add git flow and service declaration for sys-monitord

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-updater.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-updater.sh
@@ -6,13 +6,14 @@ if ! ping -q -c 1 -W 2 archlinux.org >/dev/null; then
   exit 1
 fi
 
-output=$(sudo pacman -Sy --needed --noconfirm feral-connectd feral-setupd)
+output=$(sudo pacman -Sy --needed --noconfirm feral-connectd feral-setupd feral-sys-monitord)
 
 if ! echo "$output" | grep -q "there is nothing to do"; then
   echo "Detected installation or upgrade. Reloading systemd and restarting services..."
   sudo systemctl daemon-reload
   sudo systemctl restart feral-connectd.service
   sudo systemctl restart feral-setupd.service
+  sudo systemctl restart feral-sys-monitord.service
 else
   echo "Packages already up to date. No action needed."
 fi
