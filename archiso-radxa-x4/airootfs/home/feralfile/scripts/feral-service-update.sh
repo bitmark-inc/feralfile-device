b4fd9057b817d42f53661e889e176194f57569ec
Brandon Yeh <lpopo0856@gmail.com>
2025-05-22 15:00:15 +0800

    add watchdog to update script

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-service-update.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-service-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-output=$(sudo pacman -Sy --needed --noconfirm feral-connectd feral-setupd feral-sys-monitord)
+output=$(sudo pacman -Sy --needed --noconfirm feral-connectd feral-setupd feral-sys-monitord feral-watchdog)
 
 if ! echo "$output" | grep -q "there is nothing to do"; then
   echo "Detected installation or upgrade. Reloading systemd and restarting services..."
@@ -9,6 +9,7 @@ if ! echo "$output" | grep -q "there is nothing to do"; then
   sudo systemctl restart feral-connectd.service
   sudo systemctl restart feral-setupd.service
   sudo systemctl restart feral-sys-monitord.service
+  sudo systemctl restart feral-watchdog.service
 else
   echo "Packages already up to date. No action needed."
 fi
